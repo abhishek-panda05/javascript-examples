f5dca168690e79f60741d326ba757aefcb14f455
Abhishek <luckyadi529@gmail.com>
2026-01-21 15:34:36 +0530

    Added more examples

---
+++ examples/example1.js
@@ -0,0 +1,8 @@
+/**
+ * hello this is 1st example of js
+ */
+// console.log("Hello");
+let x = 10;
+console.log(typeof x);
+const y = 10;
+console.log(y);
---
+++ examples/example3.js
@@ -0,0 +1,5 @@
+// AND
+let x = 10;
+let y = 20;
+
+console.log(x === 10 && y === 20);
