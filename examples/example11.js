3536537e4436ab13049ea7692cae09e6a3bc6b18
Abhishek <luckyadi529@gmail.com>
2026-01-22 10:46:30 +0530

    Added example11.js

---
+++ examples/example11.js
@@ -0,0 +1,10 @@
+// const add = function addNumbers(a, b) {
+//   return a + b;
+// };
+
+// const r = add(10, 20);
+// console.log(r);
+
+const add = (a, b) => a + b;
+const r = add(10, 20);
+console.log(r);
